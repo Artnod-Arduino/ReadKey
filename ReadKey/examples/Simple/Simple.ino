feece43df4d000ac3683d819b1ddbb7333306275
Artnod <artnod78@users.noreply.github.com>
2018-05-05 14:01:55 +0200

    Update Simple.ino

--- ReadKey/examples/Simple/Simple.ino
+++ ReadKey/examples/Simple/Simple.ino
@@ -6,6 +6,7 @@ ReadKey keypad(KEY_PIN);
 
 void setup()
 {	
+  Serial.begin(9600);
 }
 
 void loop()
@@ -17,18 +18,23 @@ void loop()
 		switch(keypad.key())
 		{
 			case btnRIGHT:
+				Serial.println("btnRIGHT");
 				// btnRIGHT press
 				break;
 			case btnUP:
+				Serial.println("btnUP");
 				// btnUP press
 				break;
 			case btnDOWN:
+				Serial.println("btnDOWN");
 				// btnDOWN press
 				break;
 			case btnLEFT:
+				Serial.println("btnLEFT");
 				// btnLEFT press
 				break;
 			case btnSELECT:
+				Serial.println("btnSELECT");
 				// btnSELECT press
 				break;
 			default:
